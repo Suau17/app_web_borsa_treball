1a03ad6b17eb0e43b037ae64dea287990cb343f0
HelderRivas <Helder24102003@gmail.com>
2022-10-14 09:39:47 +0200

    schema gestor/responsable hecho

---
+++ src/schemas/gestor.schema.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const {model ,Schema} = mongoose
+
+const userSchema = new Schema({
+    nom: { type: 'string', required: true, minLenght: 4,  maxLength: 20},    
+    cognoms : { type: 'string', required: true, minLenght: 4,  maxLength: 20},
+    carrec : { type: 'string', required: true, minLenght: 4,  maxLength: 20},
+    dni : { type: 'string', required: true, minLenght: 4,  maxLength: 20},
+    gestor : {type: 'boolean'}
+})
+
+const userModel = model('User', userSchema)
+
+export default userModel
