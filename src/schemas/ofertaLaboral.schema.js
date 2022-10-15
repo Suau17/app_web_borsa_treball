3b2e9bd53bf1ded3501af1dfa49ef3bbd56a38e8
Marc Gruber Lopéz <99557533+MarcGruber@users.noreply.github.com>
2022-10-15 14:04:08 +0200

    actualización del schema (15/10/2022)

--- src/schemas/ofertaLaboral.schema.js
+++ src/schemas/ofertaLaboral.schema.js
@@ -4,9 +4,9 @@ const {model ,Schema} = mongoose
 const ofertaLaboralSchema = new Schema({
     title : { type: 'string', required: true,  maxLength: 100},    
     description : { type: 'string', required: true},
-    requirements : { type: 'string', required: true, maxLength: 20},
-    skills : { type: 'string', required: true, maxLength: 20},
-    ciclo : { type: 'string', required: true, maxLength: 20},
+    requirements : { type: 'string', required: true},
+    skills : { type: 'string', required: true},
+    ciclo : { type: 'string', required: true, maxLength: 150},
     dateOfPublication : { type: 'date', required: true},
     expirationDate : { type: 'date'}
 })
