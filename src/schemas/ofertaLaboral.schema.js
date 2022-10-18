cc19e6e17d71791811c64258000f748e1d3b0d30
Marc Gruber Lopéz <99557533+MarcGruber@users.noreply.github.com>
2022-10-18 17:11:21 +0200

    update ofertaLaboral

--- src/schemas/ofertaLaboral.schema.js
+++ src/schemas/ofertaLaboral.schema.js
@@ -6,9 +6,10 @@ const ofertaLaboralSchema = new Schema({
     description : { type: 'string', required: true},
     requirements : { type: 'string', required: true},
     skills : { type: 'string', required: true},
-    ciclo : { type: 'string', required: true, maxLength: 150},
+    ciclo : { typse: 'string', required: true, maxLength: 150},
     dateOfPublication : { type: 'date', required: true},
-    expirationDate : { type: 'date'}
+    expirationDate : { type: 'date'},
+    createBy : { type: 'string', required: true, maxLength: 150},
 })
 
 const ofertaLaboral = model('ofertaLaboral', ofertaLaboralSchema)
