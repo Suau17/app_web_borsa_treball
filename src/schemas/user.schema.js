6cbcf6339af567215f0ee4dffc2cf258a6ba925d
Suau <gsuau.17@gmail.com>
2022-10-19 09:13:33 +0200

    arreglo

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -2,13 +2,9 @@ import mongoose from "mongoose";
 const {model ,Schema} = mongoose
 
 const userSchema = new Schema({
-    dni : { type: 'string', required: true, minLenght: 9,  maxLength: 9},    
-    nom : { type: 'string', required: true},
-    cognoms : { type: 'string', required: true},
-    data_naixement : { type: 'date', required: true},
-    carnet : { type: 'boolean', required: true},
-    estudis : { type: 'string', required: true},
-    cv : { type: 'boolean', required: true}
+    name : { type: 'string', required: true, minLenght: 4,  maxLength: 20},    
+    description : { type: 'string', required: true, minLenght: 4,  maxLength: 20},
+    password : { type: 'string', required: true, minLenght: 4,  maxLength: 20}
 })
 
 const userModel = model('User', userSchema)
