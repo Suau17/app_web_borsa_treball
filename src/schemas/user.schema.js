9a5fdf0b5de89b7ebcc4c01bf36d95eb3bfc0ffc
Suau <gsuau.17@gmail.com>
2022-10-19 08:29:46 +0200

    schema alumnes

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 const {model ,Schema} = mongoose
 
 const userSchema = new Schema({
-    name : { type: 'string', required: true, minLenght: 4,  maxLength: 20},    
-    description : { type: 'string', required: true, minLenght: 4,  maxLength: 20},
-    password : { type: 'string', required: true, minLenght: 4,  maxLength: 20}
+    dni : { type: 'string', required: true, minLenght: 9,  maxLength: 9},    
+    nom : { type: 'string', required: true},
+    cognoms : { type: 'string', required: true},
+    data_naixement : { type: 'date', required: true},
+    carnet : { type: 'boolean', required: true},
+    estudis : { type: 'string', required: true},
+    cv : { type: 'boolean', required: true}
 })
 
 const userModel = model('User', userSchema)
