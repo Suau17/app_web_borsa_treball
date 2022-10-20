219123203fb853f56c10c8c96c0cbddb79ae2ff2
Marc Gruber Lopéz <99557533+MarcGruber@users.noreply.github.com>
2022-10-20 10:48:13 +0200

    h

---
+++ src/routes/borsa.routes.js
@@ -0,0 +1,16 @@
+import userRegistrerController from '#controllers/oferta-register.controller.js'
+import {Router} from 'express'
+const userRouter = Router()
+
+userRouter.get('/:', (req, res)=>{
+    res.send("hola")
+})
+userRouter.post('/register', userRegistrerController) 
+userRouter.post('/login')
+userRouter.post('/profile')
+userRouter.patch('/update-data')  
+userRouter.patch('/update-email')
+userRouter.patch('/update-password')
+userRouter.delete('/unregister')
+
+export default userRouter
