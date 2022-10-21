e28c4c008823460d2b2a52ca43163d2e7aded72f
Marc Gruber Lopéz <99557533+MarcGruber@users.noreply.github.com>
2022-10-21 08:30:18 +0200

    a

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,38 @@
+import UserModel from "#schemas/User.js"
+// importamos el hash para encriptar
+ import {hash} from 'bcrypt'
+const userRegistrerController = async (req, res) => {
+    const { _id ,name, description, email, passwordHash, rolUser} = req.body
+
+    const exsistingUserByEmail = await UserModel.findById(email).exec()
+
+    if (exsistingUserByEmail) return res.status(499).send('ya exsiste un usuario con ese email registrado')    
+   // cogemos la variable que viene del req.body y la encriptamos
+    const hashedPassword = await  hash(passwordHash, 12) 
+
+   const user = new UserModel({
+        _id, 
+        name,
+        description,
+        email, 
+        // asignamos la contraseña encriptada
+        passwordHash : hashedPassword,
+        rolUser
+    })
+    await user.save()
+
+return res.send('usuario registrado con exito')
+}
+
+
+/* const userLoginController = async (req, res) => {
+    const {_id, email, passwordHash} = req.body
+
+    // comprobamos que el email exsiste en la DB
+    const exsistingUserByEmail = await UserModel.findById(email).exec()
+    if (!exsistingUserByEmail) return res.status(499).send('este email/usuario no esta registrado')  
+    
+
+} */
+
+export default userRegistrerController
