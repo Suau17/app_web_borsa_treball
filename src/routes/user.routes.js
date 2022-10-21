a71260b77c5d0b8ff829ee706b1273f033f9c98e
Marc Gruber Lopéz <99557533+MarcGruber@users.noreply.github.com>
2022-10-21 08:30:18 +0200

    a

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,4 @@
-import userRegistrerController from '#controllers/user-registrer.controller.js'
+import userRegistrerController from '#controllers/user.controller.js'
 import {Router} from 'express'
 
 const userRouter = Router()
