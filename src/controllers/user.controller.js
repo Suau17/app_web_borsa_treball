726afc4f3682c6e17f600bf9f4ba954ccc4e00a8
HelderRivas <Helder24102003@gmail.com>
2022-10-26 10:25:01 +0200

    as

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -43,19 +43,19 @@ export const userLoginController = async (req, res) => {
 
 
 export const getUsersControllers = (req, res) => {
-
+    
     UserModel.find().exec(function async (err, list_users) {
         if (err) {
             return next(err);
         }
         // en la view saldara una var con json list_users
-      res.send({ listaUsuarios: list_users })
+      //res.send({ listaUsuarios: list_users })
       
        
             // 'await' espera a que trobi les dades de Genere. Amb 'await' es obligat posar 'async' a la
             // definició del mètode. El 'await' sempre ha d'estar entre un 'try-catch'
             
-       //    res.render('users/list',{listaUsuarios: list_users})   
+      res.render('usersViews/list',{listaUsuarios: list_users})   
           
         
     })
