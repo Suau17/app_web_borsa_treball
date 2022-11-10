c1e651b681917c0d4aa15c4c9848ba8744d4b1b1
HelderRivas <helder24102003@gmail.com>
2022-11-10 09:09:25 +0100

    borsa

--- src/routes/borsa.routes.js
+++ src/routes/borsa.routes.js
@@ -1,22 +1,23 @@
-import * as oferta from '#controllers/oferta.controller.js'
-import {Router} from 'express'
+import * as oferta from '#controllers/oferta.controller.js';
+import {Router} from 'express';
 
-const userRouter = Router()
+const userRouter = Router();
 
 
 userRouter.get('/', oferta.getOfertasController) ;
 
-userRouter.post('/borsa/registrar', function (req, res) {
-    res.render('ofertas/registrar.ejs');  
+userRouter.post('/borsa/registrar', function (req, res){
+    res.render('ofertas/registrar.ejs'); 
+}); 
 
 userRouter.post('/crear', function (req, res) {
     res.render('ofertas/registrar') ;
 
 
 }) 
-userRouter.post('/borsa/registerOferta', oferta.ofertaRegisterController) 
-userRouter.get('/borsa/getOfertas', oferta.getOfertasController) 
-userRouter.patch('/borsa/update-data')  
+userRouter.post('/borsa/registerOferta', oferta.ofertaRegisterController) ;
+userRouter.get('/borsa/getOfertas', oferta.getOfertasController) ;
+userRouter.patch('/borsa/update-data');
 
 
 
