0be75ce6be42b3ff579c7e8abb8f3f354ff28c56
MarcGruber <mgruberlopez@gmail.com>
2022-11-16 08:58:45 +0100

    fix post  Users (form register)

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,7 +10,7 @@ userRouter.get('/getUsers',  userC.getUsersControllers ) // funciona (json no vi
 userRouter.get('/register', function (req, res){
     res.render('usersView/register'); 
 }); 
-userRouter.post('/register', userC.userRegistrerController) // funciona
+userRouter.post('/registerUser', userC.userRegistrerController) // funciona
 userRouter.post('/login', userC.userLoginController ) 
 userRouter.post('/profile')
 userRouter.patch('/update-data')  
