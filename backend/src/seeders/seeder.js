aad5dfa7aa71cfd2dcdd14504ed0a936cfabbe55
Suau <gsuau.17@gmail.com>
2023-02-03 09:20:14 +0100

    algo

--- backend/src/seeders/seeder.js
+++ backend/src/seeders/seeder.js
@@ -1,46 +1,58 @@
-import * as fs from 'fs'
-import * as dotenv from 'dotenv'
-import bcrypt from 'bcrypt'
+import fs from 'fs';
+import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import * as path from 'path';
-import users from "#schemas/User.js";
+import User from "#schemas/User.js"
+import GestorModel from "#schemas/Gestor.js"
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const connectionString = 'mongodb+srv://marc:Qwerty1234@cluster0.wxnspxr.mongodb.net/pruebaBorsa?retryWrites=true&w=majority';
-mongoose.set('strictQuery', false);
-mongoose.connect(connectionString);
-
-//node .\src\seeders\seeder.js per a fer anar el seeder
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', async () => {
-  console.log('Connected to MongoDB');
-
-  // Load data from a JSON file
-  console.log(__dirname)
-  const user = JSON.parse(fs.readFileSync(__dirname+'/users.json'));
-  console.log(data);
-
-    /*for(var i =0; i<  users.length; i ++) {
-    users.users[i].password =  await bcrypt.hash(users.users[i].password,12);
-  }
-
-  console.log('Data encrypted successfully');*/
-
-  try {
-    // Insert data into the database
-    // booksJSON.books[0].genre = [genres[0].id,genres[1].id];
-    const usuaris = await users.insertMany(data);
-    console.log('Data seeded successfully');
-    mongoose.connection.close();
-  } catch (error) {
-    console.error(error);
-  }
-
-
+// Trobar l'arxiu .env
+console.log(dotenv.config())
+// Conectar-se a la database
+console.log('conexion'+process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
 });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to MongoDB!");
+});
+// Llegir els arxius JSON
+const users = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '/users.json'), 'utf8')
+);
+const gestor = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '/gestor.json'), 'utf8')
+);
+
+const importData = async () => {
+    try {
+        // Crear usuarios
+        const createdUsers = await User.create(users);
+        // Recorres todos los usuarios y comprobamos si es un gestor
+        console.log(createdUsers)
+        createdUsers.forEach(user => {
+            if (user.rolUser === "gestor") {
+                // Recorres todos los objetos de gestor 
+                gestor.forEach(g => {
+                    // Si refUser esta vacio lo asignamos
+                    if(!g.refUser) {
+                        g.refUser = user._id;
+                    }
+                });
+            }
+        });
+        // aquí podrías guardar los cambios en gestor.json
+        const createdGestores = await GestorModel.create(gestor);
+        console.log("Dades importades...");
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+importData();
