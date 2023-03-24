8cd621557532d137b95b7246690a165acaa92709
HelderRivas <helder24102003@gmail.com>
2023-03-24 10:39:03 +0100

    asassa

--- backend/src/routes/app.routes.js
+++ backend/src/routes/app.routes.js
@@ -2,6 +2,7 @@ import * as oferta from '#controllers/oferta.controller.js';
 // RUTAS PARA OBTENER DATOS Y ALIMENTAR EL FRONTEND
 import * as userC from '#controllers/user.controller.js'
 import * as gestor from '#controllers/gestorController.js'
+import * as empresa from '#controllers/empresa.controller.js'
 import {Router} from 'express';
 
 const appRouter = Router();
@@ -13,6 +14,7 @@ const appRouter = Router();
 appRouter.get('/getUsers',   userC.getUsersControllers)
 appRouter.get('/getOfertas', oferta.getOfertasController) 
 appRouter.get('/getOfertas/:id', oferta.getOfertaEmpresaController) 
+appRouter.get('/getEmpresa', empresa.getEmpresaControllers)
 
 
 appRouter.post('/register', gestor.gestorRegistrerController)
