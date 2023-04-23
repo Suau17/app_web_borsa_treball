c8043d7eeb98325e3deaaaedfe4a714b87e67ab7
HelderRivas <helder24102003@gmail.com>
2023-04-23 18:06:14 +0200

    home style

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -6,12 +6,18 @@ function Home() {
   return (
     <>
     {/* <Menu /> */}
-     <h1 className='home'>HOME BORSA DE TREBALL </h1>
-     <h2>INSTITUT VIDAL I BARRAQUER</h2>
-
+     <h1 className='home'>BORSA DE TREBALL </h1>
+     <h2 className='vidal'>INSTITUT VIDAL I BARRAQUER</h2>
+    <div className='generalDiv'>
      <div className='infoEmpresa'><p>aosdpaodjap</p></div>
-     <div><p>aosdpaodjap</p></div>
+     <div className='infoUser'>
+       <h3>Estudiantes</h3>
+       <p><a href='/register'>Registrate y no te pieras ninguna oferta</a> </p>
+       <p><a href="/getOfertas">ver las ofertas</a></p>
+       </div>
+     </div>
 
+     <div className='imagenes'>imagenes empresas</div>
      <footer>
       <p>Vidal I Barraquer</p>
      </footer>
