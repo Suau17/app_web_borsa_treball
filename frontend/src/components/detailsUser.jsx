d6a8ff26e4f5885dd765b0d476611f9eec869ce9
MarcGruber <mgruberlopez@gmail.com>
2023-05-05 21:07:03 +0200

    edit details user

--- frontend/src/components/detailsUser.jsx
+++ frontend/src/components/detailsUser.jsx
@@ -2,46 +2,49 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { GetUser } from "../services/app/getUser";
 import '../assets/perfil.css'
-export  function DetailsUser() {
+export function DetailsUser() {
     let [profile, setProfile] = useState([])
     const { id } = useParams();
     useEffect(() => {
-        GetUser(id).then(profile => setProfile(profile))       
+        GetUser(id).then(profile => setProfile(profile))
     }, [])
     console.log('PROFILEEE')
-   
+
     let html;
-    if(profile.user){
-      const data = profile.alumno[0].curriculum.data
-         html = (
+    if (profile.user) {
+        const data = profile.alumno[0].curriculum.data
+        html = (
             <>
-            <div className="card">
-            <div className="img">
-                <img src="public\img\usuario.png" alt="" />
-            </div>
-            <div className="content">
-            <h2>{profile.user.name}</h2>
-            <div className="center">
-                    <div className="box">
-                    <span className=" " >Email: {profile.user.email}</span>
+                <div className="card">
+                    <div className="img">
+                        <img src="public\img\usuario.png" alt="" />
                     </div>
-                    
+                    <div className="content">
+                        <h2>{(profile.user.name).toUpperCase()}</h2>
+                        <div className="center">
+                            <div className="box">
+                                <span  >Email: {profile.user.email}</span><br />
+                                <span><a href = {"mailto: "+profile.user.email} class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Send Email</a></span>
+                                <br />
+                                <span ><a href={profile.alumno[0].link} class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Pagina personal</a> </span>
+                            </div>
+
+                        </div>
+                        <a target="_blank" href={`${import.meta.env.VITE_URL}/app/curriculum/${profile.alumno[0]._id}`} download><button className="btn">CV</button></a> 
                     </div>
-                    <button className="btn"><a target="_blank" href={`${import.meta.env.VITE_URL}/app/curriculum/${profile.alumno[0]._id}`} download>CV</a> </button>
+
+
+
+
+
+
                 </div>
-                
-            
-                
-               
-            
-            
-            </div>
             </>
         )
     } else {
-         html = (
+        html = (
             <>
-            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+                <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
             </>
         )
     }
