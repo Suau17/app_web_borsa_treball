943d35a1a836186f802e70c0e892e16a99a33aef
HelderRivas <helder24102003@gmail.com>
2023-05-05 11:15:35 +0200

    hemos acabado ya?

--- frontend/src/services/register.jsx
+++ frontend/src/services/register.jsx
@@ -177,7 +177,7 @@ export async function RegisterAlumno(props) {
                 </li>
               ))}
           </ul>
-          <button onClick={() => toast.dismiss(t)}>close</button>
+          <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => toast.dismiss(t)}>close</button>
         </div>
       ));
     }
